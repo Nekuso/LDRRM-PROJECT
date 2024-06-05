2a1044003ec95c379327d47c958178fa3ba0f64c
terrencedante6 <159745896+terrencedante6@users.noreply.github.com>
2024-06-05 12:38:25 +0800

    Polishing the request button
    - Next is adding the timer delay on search location

--- src/app/application/requests/transactions-table/orders-table/add-order/add-order-table/products-options/columns.tsx
+++ src/app/application/requests/transactions-table/orders-table/add-order/add-order-table/products-options/columns.tsx
@@ -23,9 +23,9 @@ type option = {
 
 export const initiateColumns = (dispatch: any, foodsuppliesCart: any) => {
   const columns: ColumnDef<option>[] = [
+    // ... other columns
     {
-      id: "name",
-      accessorKey: "name",
+      id: "vehicles",
       header: "Food Supply",
       cell: ({ row }) => {
         return (
@@ -44,9 +44,6 @@ export const initiateColumns = (dispatch: any, foodsuppliesCart: any) => {
               <p className="text-xs max-w-[100px] 2xl:max-w-[200px] truncate font-semibold ">
                 {row.original.name}
               </p>
-              {/* <p className="text-sx max-w-[120px] 2xl:max-w-[180px] truncate text-white font-bold">
-                {`₱ ${row.original.price} • ${row.original.brands.brand_name}`}
-              </p> */}
               <p className="text-xs max-w-[181px] truncate text-slate-500">
                 Stock:
                 <span
@@ -66,73 +63,53 @@ export const initiateColumns = (dispatch: any, foodsuppliesCart: any) => {
     },
     {
       id: "actions",
-      header: () => {
-        return <div className="w-full text-right">Action</div>;
-      },
+      header: () => <div className="text-right pr-8">Actions</div>,
       cell: ({ row }) => {
         return (
-          <div className="w-full flex gap-2 justify-end">
-            <Button
-              className={cn(
-                "text-xs font-bold rounded-md flex gap-2 hover:text-white transition-all duration-300 px-6 py-2 cursor-pointer",
-                row.original.stock_quantity === 0
-                  ? "bg-blue-600"
-                  : "bg-applicationPrimary hover:bg-applicationPrimary/70"
-              )}
-              type="button"
-              disabled={
-                foodsuppliesCart.some(
-                  (foodsupply: any) =>
-                    foodsupply.foodsupply_id === row.original.id
-                ) || row.original.stock_quantity === 0
-              }
-              onClick={() => {
-                dispatch(
-                  addFoodSupplyToCart({
-                    foodsupply_id: row.original.id,
-                    // inventory_id: row.original.inventory.id,
-                    name: row.original.name,
-                    description: row.original.description,
-                    image: row.original.image_url,
-                    quantity: 1,
-                  })
-                );
-              }}
-            >
-              <div
-                className={cn(
-                  "rounded-md text-white bg-blue-600 px-4 py-2 text-base",
-                  foodsuppliesCart.some(
-                    (foodsupply: any) =>
-                      foodsupply.foodsupply_id === row.original.id
-                  )
-                    ? "Added"
-                    : row.original.stock_quantity === 0
-                    ? "Out of Stock"
-                    : "Add"
-                )}
-              >
-                {foodsuppliesCart.some(
-                  (foodsupply: any) =>
-                    foodsupply.foodsupply_id === row.original.id
-                )
-                  ? "Added"
-                  : row.original.stock_quantity === 0
-                  ? "Out of Stock"
-                  : "Add"}
-              </div>
-            </Button>
+          <div className="w-full flex gap-4 justify-end">
             {foodsuppliesCart.some(
               (foodsupply: any) => foodsupply.foodsupply_id === row.original.id
-            ) && (
+            ) ? (
               <Button
-                className="text-xs font-bold rounded-md flex gap-2 hover:text-white transition-all duration-300 px-4 py-2 cursor-pointer"
+                className="text-xs font-bold rounded-md flex gap-2 hover:text-white transition-all duration-300 px-4 py-2 cursor-pointer bg-red-500 hover:bg-red-00"
                 type="button"
                 onClick={() => {
                   dispatch(removeFoodSupplyFromCart(row.original.id));
                 }}
               >
                 <IoMdRemove />
+              </Button>
+            ) : (
+              <Button
+                className={cn(
+                  "text-xs font-bold rounded-md flex gap-2 hover:text-white transition-all duration-300 px-6 py-2 cursor-pointer",
+                  row.original.stock_quantity === 0
+                    ? "bg-blue-600"
+                    : "bg-applicationPrimary hover:bg-applicationPrimary/70"
+                )}
+                type="button"
+                disabled={row.original.stock_quantity === 0}
+                onClick={() => {
+                  dispatch(
+                    addFoodSupplyToCart({
+                      foodsupply_id: row.original.id,
+                      // inventory_id: row.original.inventory.id,
+                      name: row.original.name,
+                      description: row.original.description,
+                      image: row.original.image_url,
+                      quantity: 1,
+                    })
+                  );
+                }}
+              >
+                <div
+                  className={cn(
+                    "rounded-md text-white bg-blue-600 px-4 py-2 text-base",
+                    row.original.stock_quantity === 0 ? "Out of Stock" : "Add"
+                  )}
+                >
+                  {row.original.stock_quantity === 0 ? "Out of Stock" : "Add"}
+                </div>
               </Button>
             )}
           </div>
--- src/app/application/requests/transactions-table/orders-table/add-order/add-order-table/vehicles-options/columns.tsx
+++ src/app/application/requests/transactions-table/orders-table/add-order/add-order-table/vehicles-options/columns.tsx
@@ -24,9 +24,8 @@ type option = {
 export const initiateColumns = (dispatch: any, vehiclesCart: any) => {
   const columns: ColumnDef<option>[] = [
     {
-      id: "name",
-      accessorKey: "name",
-      header: "Vehicle",
+      id: "vehicles",
+      header: "Vehicles",
       cell: ({ row }) => {
         return (
           <div className="flex place-items-between gap-4">
@@ -44,12 +43,9 @@ export const initiateColumns = (dispatch: any, vehiclesCart: any) => {
               <p className="text-xs max-w-[100px] 2xl:max-w-[200px] truncate font-semibold ">
                 {row.original.name}
               </p>
-              {/* <p className="text-sx max-w-[120px] 2xl:max-w-[180px] truncate text-white font-bold">
-                {`₱ ${row.original.price} • ${row.original.brands.brand_name}`}
-              </p> */}
               <p className="text-xs max-w-[181px] truncate text-slate-500">
                 Plate Number: {row.original.plate_number}
-                {/* <span
+                <span
                   className={cn(
                     "",
                     row.original.stock_quantity === 0 ? "text-red-500" : ""
@@ -57,7 +53,7 @@ export const initiateColumns = (dispatch: any, vehiclesCart: any) => {
                 >
                   {" "}
                   {row.original.stock_quantity}
-                </span> */}
+                </span>
               </p>
             </div>
           </div>
@@ -66,70 +62,53 @@ export const initiateColumns = (dispatch: any, vehiclesCart: any) => {
     },
     {
       id: "actions",
-      header: () => {
-        return <div className="w-full text-right">Action</div>;
-      },
+      header: () => <div className="text-right pr-8">Actions</div>,
       cell: ({ row }) => {
         return (
-          <div className="w-full flex gap-2 justify-end">
-            <Button
-              className={cn(
-                "text-xs font-bold rounded-md flex gap-2 hover:text-white transition-all duration-300 px-6 py-2 cursor-pointer",
-                row.original.stock_quantity === 0
-                  ? "bg-blue-600"
-                  : "bg-applicationPrimary hover:bg-applicationPrimary/70"
-              )}
-              type="button"
-              disabled={
-                vehiclesCart.some(
-                  (vehicle: any) => vehicle.vehicle_id === row.original.id
-                ) || row.original.stock_quantity === 0
-              }
-              onClick={() => {
-                dispatch(
-                  addVehicleToCart({
-                    vehicle_id: row.original.id,
-                    name: row.original.name,
-                    plate_number: row.original.plate_number,
-                    description: row.original.description,
-                    image: row.original.image_url,
-                    quantity: 1,
-                  })
-                );
-              }}
-            >
-              <div
-                className={cn(
-                  "rounded-md text-white bg-blue-600 px-4 py-2 text-base",
-                  vehiclesCart.some(
-                    (vehicle: any) => vehicle.vehicle_id === row.original.id
-                  )
-                    ? "Added"
-                    : row.original.stock_quantity === 0
-                    ? "Out of Stock"
-                    : "Add"
-                )}
-              >
-                {vehiclesCart.some(
-                  (vehicle: any) => vehicle.equipment_id === row.original.id
-                )
-                  ? "Added"
-                  : row.original.stock_quantity === 0
-                  ? "Out of Stock"
-                  : "Add"}
-              </div>
-            </Button>
+          <div className="w-full flex gap-4 justify-end items-center">
             {vehiclesCart.some(
               (vehicle: any) => vehicle.vehicle_id === row.original.id
-            ) && (
+            ) ? (
               <Button
-                className="text-xs font-bold rounded-md flex gap-2 hover:text-white transition-all duration-300 px-4 py-2 cursor-pointer"
+                className="text-xs font-bold rounded-md flex gap-2 hover:text-white transition-all duration-300 px-4 py-2 cursor-pointer bg-red-500 hover:bg-red-00"
                 type="button"
                 onClick={() => {
                   dispatch(removeVehicleFromCart(row.original.id));
                 }}
               >
                 <IoMdRemove />
+              </Button>
+            ) : (
+              <Button
+                className={cn(
+                  "text-xs font-bold rounded-md flex gap-2 hover:text-white transition-all duration-300 px-6 py-2 cursor-pointer",
+                  row.original.stock_quantity === 0
+                    ? "bg-blue-600"
+                    : "bg-applicationPrimary hover:bg-applicationPrimary/70"
+                )}
+                type="button"
+                disabled={row.original.stock_quantity === 0}
+                onClick={() => {
+                  dispatch(
+                    addVehicleToCart({
+                      vehicle_id: row.original.id,
+                      plate_number: row.original.plate_number,
+                      name: row.original.name,
+                      description: row.original.description,
+                      image: row.original.image_url,
+                      quantity: 1,
+                    })
+                  );
+                }}
+              >
+                <div
+                  className={cn(
+                    "rounded-md text-white bg-blue-600 px-4 py-2 text-base",
+                    row.original.stock_quantity === 0 ? "Out of Stock" : "Add"
+                  )}
+                >
+                  {row.original.stock_quantity === 0 ? "Out of Stock" : "Add"}
+                </div>
               </Button>
             )}
           </div>
